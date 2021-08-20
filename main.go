4d4ae893343759e9b347df0c7cc7d4fb076cce10
henry111666888 <henry111666888@gmail.com>
2021-08-20 17:19:39 +0800

    test bitra

--- main.go
+++ main.go
@@ -35,6 +35,12 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+/*
+
+測試分支
+
+*/
+
 // postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
 	var newAlbum album
