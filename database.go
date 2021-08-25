e1d74c8221ca4e9d7ae9bcaf0a93a424e3e6fdfc
henry111666888 <henry111666888@gmail.com>
2021-08-25 17:34:31 +0800

    gorm

--- database.go
+++
@@ -1,9 +0,0 @@
-package rrr
-
-import (
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-)
-
-dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
